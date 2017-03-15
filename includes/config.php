8f5406486c94fb2b372956e4742a482919949fc4
Dougal <dougal@dougalwalker.com>
2017-03-15 13:47:08 -0500

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?
+
+# App version
+$version = 1.02;
+
+# Google API settings
+$google_client_id 		= '';
+$google_client_secret 	= '';
+$google_redirect_url 	= '';
+$google_developer_key 	= '';
+
+# Database settings
+$db_username = ""; 		//database username
+$db_password = ""; 			//database password
+$hostname = ""; 			//mysql hostname
+$db_name = ''; 			//database name
