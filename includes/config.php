6a6f41cc95fcd6bbb6cdf81586673021093d5c6b
Dougal <dougal@dougalwalker.com>
2017-03-15 13:44:50 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,31 @@
-<?
-
-# App version
-$version = 1.02;
-
-# Google API settings
-$google_client_id 		= '544438716096-85nmub87d80dckgeh5a7215347tasv18.apps.googleusercontent.com';
-$google_client_secret 	= 'Bx-MiC-jU6Y3fyyYHn3JxVw_';
-$google_redirect_url 	= 'http://walkbud.com/main.php';
-$google_developer_key 	= '544438716096-85nmub87d80dckgeh5a7215347tasv18.apps.googleusercontent.com';
-
-# Database settings
-$db_username = "d_zeleftikam"; 		//database username
-$db_password = "hondaa"; 			//database password
-$hostname = "localhost"; 			//mysql hostname
-$db_name = 'd_walkbuddy'; 			//database name
+<?
+
+
+
+# App version
+
+$version = 1.02;
+
+
+
+# Google API settings
+
+$google_client_id 		= '';
+
+$google_client_secret 	= '';
+
+$google_redirect_url 	= 'http://';
+
+$google_developer_key 	= '';
+
+
+
+# Database settings
+
+$db_username = ""; 		//database username
+
+$db_password = ""; 			//database password
+
+$hostname = "localhost"; 			//mysql hostname
+
+$db_name = ''; 			//database name
